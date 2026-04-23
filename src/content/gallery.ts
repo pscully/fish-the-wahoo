e5df3d14afccb6d89ef3bb2dcb89cdd284d78d1d
Patrick Scully <10429618+pscully@users.noreply.github.com>
2026-04-22 21:13:40 -0400

    feat: add 12 gallery images from owner drop

--- src/content/gallery.ts
+++ src/content/gallery.ts
@@ -20,6 +20,18 @@ export const galleryImages: GalleryImage[] = [
   { src: '/images/gallery-13.webp', alt: 'Trophy marlin', size: 'small' },
   { src: '/images/gallery-14.webp', alt: 'Charter fishing crew', size: 'medium' },
   { src: '/images/gallery-15.webp', alt: 'Deep sea fishing Charleston SC', size: 'small' },
+  { src: '/images/bottom-fishing-charleston-fishing-charter.jpg', alt: 'Bottom fishing Charleston fishing charter', size: 'medium' },
+  { src: '/images/bottom-fishing-near-charleston.jpg', alt: 'Bottom fishing near Charleston', size: 'small' },
+  { src: '/images/deep-sea-fishing-charleston-sc.png', alt: 'Deep sea fishing Charleston SC', size: 'large' },
+  { src: '/images/fish-the-wahoo-charleston-sc.jpg', alt: 'Fish The Wahoo Charleston SC', size: 'medium' },
+  { src: '/images/fishing-charter-near-charleston.jpg', alt: 'Fishing charter near Charleston', size: 'small' },
+  { src: '/images/charleston-fishing-2020-07-13.jpg', alt: 'Charleston fishing trip July 2020', size: 'small' },
+  { src: '/images/charleston-fishing-2023-06-05.jpg', alt: 'Charleston offshore fishing trip', size: 'medium' },
+  { src: '/images/charleston-fishing-2023-06-05-a.jpg', alt: 'Charleston sportfishing catch', size: 'small' },
+  { src: '/images/charleston-fishing-2023-06-05-b.jpg', alt: 'Gulf Stream fishing Charleston', size: 'small' },
+  { src: '/images/charleston-fishing-gallery-01.jpg', alt: 'Charleston deep sea fishing', size: 'small' },
+  { src: '/images/charleston-fishing-gallery-02.jpg', alt: 'Charleston fishing charter trip', size: 'small' },
+  { src: '/images/charleston-fishing-gallery-03.jpg', alt: 'Charleston charter fishing catch', size: 'medium' },
 ];
 
 export const previewImages = galleryImages.slice(0, 8);
